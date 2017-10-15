29e65d5bac5cd1ebc84d96d7ac86798ad0534d0a
Ryo Nakagome <nakagome@tento-net.com>
2017-10-15 13:22:55 +0900

    基本メッセージを追加

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+	@EventHandler
+	public void onLoad() {
+		getLogger().info("Suicraプラグインをロード中です・・・");
+	}
+
 	@EventHandler
 	public void onEnable() {
-
+		getLogger().info("Suicraプラグインが有効になりました！");
 	}
 
 	@EventHandler
 	public void onDisable() {
-
+		getLogger().info("Suicraプラグインが無効になりました！");
 	}
 }
