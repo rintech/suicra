9aa0d37fd0c2d2f0b00df17654ad50514da89e4d
Ryo Nakagome <nakagome@tento-net.com>
2017-10-15 13:17:07 +0900

    とりあえず基本ファイルを作成 Lodge楽しい

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	@EventHandler
+	public void onEnable() {
+
+	}
+
+	@EventHandler
+	public void onDisable() {
+
+	}
+}
