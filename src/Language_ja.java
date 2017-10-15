70cf3d60bd04f4e4ea7b1a2972bd08ee4c12a4c2
Ryo Nakagome <nakagome@tento-net.com>
2017-10-15 16:25:10 +0900

    よくわからん

--- src/Language_ja.java
+++ src/Language_ja.java
@@ -5,7 +5,14 @@ public class Language_ja {
 	public static String onEnable = "Suicraプラグインが有効になりました！";
 	public static String onDisable = "Suicraプラグインが無効になりました！";
 
+	public static String param_is_not_enough = "パラメータが足りません。";
+	public static String param_wrong_cmd = "パラメータが間違っています。";
+	public static String cmd_only_player = "このコマンドはプレイヤーのみが実行できます。";
+
 	public static String Include_Suicra_isOP = "Suicraプラグインが導入されています。";
 	public static String Include_suicra = "このサーバにはSuicraプラグインが導入されています。ICカードに手持ちのお金をチャージすることで鉄道路線を利用できます。";
+
+	//History Mode
+	public static String History_Header = "--- Suicra ご利用明細 ---";
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -5,21 +7,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-	@EventHandler
+	@Override
 	public void onLoad() {
 		getLogger().info(Language_ja.onLoad);
 	}
 
-	@EventHandler
+	@Override
 	public void onEnable() {
 		getLogger().info(Language_ja.onEnable);
 	}
 
-	@EventHandler
+	@Override
 	public void onDisable() {
 		getLogger().info(Language_ja.onDisable);
 	}
 
+	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		if (player.isOp()) {
@@ -29,8 +32,17 @@ public class Main extends JavaPlugin {
 		}
 	}
 
-	public void setKaisatsu() {
-
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args, Language_ja lang) {
+		if(cmd.getName().equalsIgnoreCase("sc")) {
+			if(args.length == 0) {
+				sender.sendMessage(lang.param_is_not_enough);
+			} else if (args[0].equalsIgnoreCase("history")) {
+				sender.sendMessage(lang.History_Header);
+			}
+			return true;
+		}
+		return false;
 	}
 
 }
