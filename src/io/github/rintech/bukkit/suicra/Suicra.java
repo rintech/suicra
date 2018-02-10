87a1394ea220158cc48e879ea7047336a4156725
電車君 <densyakun1128@gmail.com>
2018-02-11 02:20:16 +0900

    セーブ処理を途中まで書いた。サーバー停止時又はプレイヤー退出時にセーブされる。

--- src/io/github/rintech/bukkit/suicra/Suicra.java
+++ src/io/github/rintech/bukkit/suicra/Suicra.java
@@ -1,9 +1,15 @@
 package io.github.rintech.bukkit.suicra;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.iCo6.Constants.Nodes;
@@ -12,19 +18,36 @@ import com.iCo6.util.Common;
 
 public class Suicra extends JavaPlugin implements Listener {
 
-	@Override
+	public static File chargesFile;
+	public static HashMap<UUID, Integer> charges = new HashMap<UUID, Integer>();
+	private boolean save = false;
+
+	/*@Override
 	public void onLoad() {
 		getLogger().info(Language_ja.onLoad);
-	}
+	}*/
 
 	@Override
 	public void onEnable() {
+		getDataFolder().mkdirs();
+		if (chargesFile.exists()) {
+			//TODO ろーど
+		} else {
+			try {
+				(chargesFile = new File(getDataFolder(), "charges.dat")).createNewFile();
+			} catch (IOException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+		}
+		//TODO -_-
 		getServer().getPluginManager().registerEvents(this, this);
 		getLogger().info(Language_ja.onEnable);
 	}
 
 	@Override
 	public void onDisable() {
+		save();
 		getLogger().info(Language_ja.onDisable);
 	}
 
@@ -38,18 +61,39 @@ public class Suicra extends JavaPlugin implements Listener {
 		}
 	}
 
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		save();
+	}
+
+	public void saveCharges() {
+		save = true;
+	}
+
 	/**
 	 * ICカードにチャージします
 	 * @param player プレイヤー
 	 * @param charge チャージ金額
 	 */
 	public void charge(Player player, int charge) {
-		//TODO チャージ残高を記録
+		Integer c = charges.get(player.getUniqueId());
+		if (c == null)
+			c = 0;
+		charges.put(player.getUniqueId(), c + charge);
+		saveCharges();
 
 		Account account = new Account(player.getName());
 		account.getHoldings().subtract(charge);
 
 		player.sendMessage(Language.PREFIX + Language_ja.charge.replaceAll(Language.KEY_CHARGE, "" + charge +
 				(String)Nodes.Major.getStringList().get(Common.plural(Double.valueOf(charge)))));
+	}
+
+	//データが変更されたらセーブを予約し、
+	//プレイヤーがログアウトまたはプラグインが無効になるときにセーブされる。
+	private void save() {
+		if (save) {
+
+		}
 	}
 }
